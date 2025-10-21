892764a6376a1745c875742956422d22c47f4588
CruzEzequiel <cruze2061@gmail.com>
2025-10-20 22:16:06 -0600

    extracction

--- controllers/syntage_data_controller.py
+++ controllers/syntage_data_controller.py
@@ -474,3 +474,31 @@ async def get_summary(entity_id: str):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error fetching data: {e}")
    
+
+@router.get("/extractions")
+async def get_extractions():
+    try:
+        api_key = os.getenv("SYNTAGE_API_KEY")
+        if not api_key:
+            raise HTTPException(status_code=500, detail="API key not configured")
+
+        url = f"https://api.sandbox.syntage.com/entities"
+        headers = {"X-API-Key": api_key,"accept-language": "es"}
+        
+        # Verificar cache
+        cached_data = cache.get(url)
+        if cached_data is not None:
+            return cached_data
+        
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url, headers=headers)
+            response.raise_for_status()
+            data = response.json()
+            # Cachear la respuesta
+            cache.set(url, data)
+            return data
+    except httpx.HTTPStatusError as e:
+        raise HTTPException(status_code=e.response.status_code, detail=f"Error from external API: {e}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error fetching data: {e}")
+   
